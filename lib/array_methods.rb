4d1e9ac61de85d08df51503ed1fbe4d083af0924
Renee France <renee5.0@outlook.com>
2019-07-17 17:10:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,4 +11,13 @@ end
 def find_min_value(array)
   min = 0
     array.length.times do |count|
+           if count == 0
+        min = array[count]
+      else
+        if array[count] < min
+          min = array[count]
+        end
+      end
+    end
+  min
 end
